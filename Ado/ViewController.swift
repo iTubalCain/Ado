35c1fc8b2dc3fc4d69c258d6cfb57da282059fe2
Tubal Cain <Will@zeus.local>
2018-10-07 18:54:34 +0800

    Initial Commit

---
+++ Ado/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ado
+//
+//  Created by Tubal Cain on 10/7/18.
+//  Copyright © 2018 Milliwaze Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
